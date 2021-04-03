206a53adf0521f995c4e8fbe60a4d5364ef66824
donald vallejo <donny.vallejo@gmail.com>
2021-04-02 23:28:45 -0700

    attempting: Users can submit and save posts

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ const express = require('express');
 
 // App Setup
 const app = express();
+require('./controllers/posts.js')(app);
+
+// Set db
+require('./data/reddit-db');
 
 // Middleware
 const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
 
 app.use(express.static('public'));
 
@@ -14,9 +27,13 @@ app.set('view engine', 'handlebars');
 app.get('/', (req, res) => {
     console.log("hemoo")
     res.render('home')
-    
+
 })
 
+app.get('/posts/new', (req, res) => {
+    console.log('posts new test')
+    res.render('posts-new')
+})
 
 
 app.listen(3000, () => {
