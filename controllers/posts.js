b5c20de446124a9848ee8e0875d015df2461a868
donald vallejo <donny.vallejo@gmail.com>
2021-04-11 18:26:54 -0700

    lost on page3

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,25 +4,36 @@ module.exports = (app) => {
 
 app.get('/', (req, res) => {
     Post.find({}).lean()
-        .then(posts => {
+      .then(posts => {
         res.render('posts-index', { posts });
-        })
-        .catch(err => {
+      })
+      .catch(err => {
         console.log(err.message);
-        })
-    })
+      })
+  })
 
-app.post('/posts/new', (req, res) => {
-// INSTANTIATE INSTANCE OF POST MODEL
-console.log("req.body",req.body)
-const post = new Post(req.body);
-console.log("new post", post)
-// SAVE INSTANCE OF POST MODEL TO DB
-post.save((err, post) => {
-//   REDIRECT TO THE ROOT
-    console.log("post saved", post)
-    return res.redirect(`/`);
-})
+  app.post('/posts/new', (req, res) => {
+    // INSTANTIATE INSTANCE OF POST MODEL
+    console.log("req.body",req.body)
+    const post = new Post(req.body);
+    console.log("new post", post)
+    // SAVE INSTANCE OF POST MODEL TO DB
+    post.save((err, post) => {
+    //   REDIRECT TO THE ROOT
+        console.log("post saved", post)
+        return res.redirect(`/`);
+    })
 }); 
+
+app.get("/posts/:id", function(req, res) {
+    // LOOK UP THE POST
+    Post.findById(req.params.id).lean()
+      .then(post => {
+        res.render("posts-show", { post });
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  });
 
 };
