1964a765981e4da03ec5290bd61934080f20fbdd
donald vallejo <donny.vallejo@gmail.com>
2021-05-01 20:28:40 -0700

    users can log in

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require("dotenv").config();
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 var cookieParser = require('cookie-parser');
@@ -29,6 +30,20 @@ app.use(cookieParser());
 require('./controllers/post.js')(app);
 require('./controllers/comments.js')(app);
 require('./controllers/auth.js')(app);
+
+var checkAuth = (req, res, next) => {
+    console.log("Checking authentication");
+    if (typeof req.cookies.nToken === "undefined" || req.cookies.nToken === null) {
+      req.user = null;
+    } else {
+      var token = req.cookies.nToken;
+      var decodedToken = jwt.decode(token, { complete: true }) || {};
+      req.user = decodedToken.payload;
+    }
+  
+    next();
+  };
+  app.use(checkAuth);
 
 app.listen(3000, () => {
     console.log('Reddit Clone listening on port localhost:3000!');
