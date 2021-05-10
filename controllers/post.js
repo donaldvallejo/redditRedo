16e75234c091ef381b151d508a2f8503b6839941
donald vallejo <donny.vallejo@gmail.com>
2021-05-10 14:52:50 -0700

    attempting Authors for comments are visible

--- controllers/post.js
+++ controllers/post.js
@@ -34,13 +34,19 @@ app.get('/posts/new', (req, res) => {
   res.render('posts-new')
 })
 
-app.get("/posts/:id", function(req, res) {
+// SHOW
+app.get("/posts/:id", function (req, res) {
+  var currentUser = req.user;
   // LOOK UP THE POST
-    Post.findById(req.params.id).lean().populate('comments').then((post) => {
-      res.render('posts-show', { post })
-    }).catch((err) => {
-      console.log(err.message)
-    })
+
+  Post.findById(req.params.id).lean()
+    .populate({path:'comments', populate: {path: 'author'}}).populate('author')
+      .then(post => {
+          res.render("posts-show", { post, currentUser });  
+      })
+      .catch(err => {
+          console.log(err.message);
+      });
 });
 
 // SUBREDDIT
