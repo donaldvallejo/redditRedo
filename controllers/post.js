9e240cffa27214a922b5cd3310eaf74ee7eabe98
donald vallejo <donny.vallejo@gmail.com>
2021-05-10 16:14:08 -0700

    refactored populate code

--- controllers/post.js
+++ controllers/post.js
@@ -37,10 +37,7 @@ app.get('/posts/new', (req, res) => {
 // SHOW
 app.get("/posts/:id", function (req, res) {
   var currentUser = req.user;
-  // LOOK UP THE POST
-
-  Post.findById(req.params.id).lean()
-    .populate({path:'comments', populate: {path: 'author'}}).populate('author')
+  Post.findById(req.params.id).populate('comments').lean()
       .then(post => {
           res.render("posts-show", { post, currentUser });  
       })
@@ -51,13 +48,12 @@ app.get("/posts/:id", function (req, res) {
 
 // SUBREDDIT
 app.get("/n/:subreddit", function (req, res) {
-  var currentUser = req.user;
-  Post.find({ subreddit: req.params.subreddit }).lean().populate('author')
-      .then(posts => {
-          res.render("posts-index", { posts, currentUser });
-      })
-      .catch(err => {
-          console.log(err);
-      });
-});
-};
+    var currentUser = req.user;
+    Post.find({ subreddit: req.params.subreddit }).lean()
+        .then(posts => {
+            res.render("posts-index", { posts, currentUser });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+})};
--- models/post.js
+++ models/post.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Populate = require("../util/autopopulate");
 
 const PostSchema = new Schema({
     title: { type: String, required: true },
@@ -10,4 +11,7 @@ const PostSchema = new Schema({
     author : { type: Schema.Types.ObjectId, ref: "User", required: true }
 }, {timestamps: {createdAt: 'created_at'}});
 
+PostSchema
+    .pre('findOne', Populate('author'))
+    .pre('find', Populate('author'))
 module.exports = mongoose.model("Post", PostSchema);
